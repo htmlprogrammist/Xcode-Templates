a87e97ac40f86e2ff15fc6a5d901afcfaefb42bb
Egor Badmaev <badmaeve2511@gmail.com>
2022-08-05 16:39:40 +0300

    feat: add `Models` to the VIP template
    `Request`, `Response`, `ViewModel`

---
+++ VIP/Template/VIP.xctemplate/___FILEBASENAME___Models.swift
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+enum ___VARIABLE_ModuleName___ {
+    struct Request {
+    }
+    struct Response {
+    }
+    struct ViewModel {
+    }
+}
